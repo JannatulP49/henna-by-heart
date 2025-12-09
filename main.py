6128f20ae7a9f5ee7b68234716757235cf37cc19
JannatulP49 <prityjannatul49@gmail.com>
2025-12-09 10:50:37 -0500

    products

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from flask import Flask, render_template
+import pymysql
+import pymysql.cursors 
+from dynaconf import Dynaconf
 
 app = Flask(__name__)
+
+config = Dynaconf(settings_file=["settings.toml"])
+
+def connect_db():
+    conn = pymysql.connect(
+        host="db.steamcenter.tech", 
+        user="jprity",
+        password= config.password,
+        database="jprity_henna_by_heart",
+        autocommit= True,
+        cursorclass= pymysql.cursors.DictCursor
+    )
+    return conn
+
 
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def browse():
+    connection = connect_db()
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `Product` ")
+    result = cursor.fetchall()
+
+
+
+    connection.close()
+    return render_template("browse.html.jinja")
