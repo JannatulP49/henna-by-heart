dd72135f226f3ffc2d94f4bed4bcd3d01a58dc56
JannatulP49 <prityjannatul49@gmail.com>
2025-12-11 10:50:32 -0500

    products

--- main.py
+++ main.py
@@ -30,8 +30,14 @@ def browse():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM `Product` ")
     result = cursor.fetchall()
-
-
-
     connection.close()
     return render_template("browse.html.jinja", products=result)
+
+@app.route("/product/<product_id>")
+def product_page(product_id):
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `Product` WHERE `ID` = %s", (product_id))
+    result = cursor.fetchone()
+    connection.close()
+    return render_template("product.html.jinja", product=result)
