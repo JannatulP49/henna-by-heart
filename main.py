9414adb5e06dcc0edeffb22531156ea1b461fabc
JannatulP49 <prityjannatul49@gmail.com>
2025-12-12 10:46:16 -0500

    login-register

--- main.py
+++ main.py
@@ -41,3 +41,11 @@ def product_page(product_id):
     result = cursor.fetchone()
     connection.close()
     return render_template("product.html.jinja", product=result)
+
+@app.route("/login")
+def login():
+    return render_template("login.html.jinja")
+
+@app.route("/register")
+def register():
+    return render_template("register.html.jinja")
